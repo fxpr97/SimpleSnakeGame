b4b99e425b2ed75b012c4d9cdd6587d182990c72
Francisco Pomaya-Ramos <franxavier1997@hotmail.com>
2022-06-09 18:18:13 -0400

    Added Classes and Methods

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,9 +3,7 @@ public class SnakeGame {
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		System.out.println("Committing");
-		System.out.println("Committing again");
+		new GameFrame();
 	}
 
 }
